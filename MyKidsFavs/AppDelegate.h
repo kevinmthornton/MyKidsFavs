ea307313b967c4eb63bb15f1e0145812939d9424
Kevin Thornton <kevinthornton@Kevins-MacBook-Pro.local>
2013-08-03 20:33:41 -0700

    Initial Commit

---
+++ MyKidsFavs/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MyKidsFavs
+//
+//  Created by Kevin Thornton on 8/3/13.
+//  Copyright (c) 2013 Kevin Thornton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MyKidsFavs/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyKidsFavs
+//
+//  Created by Kevin Thornton on 8/3/13.
+//  Copyright (c) 2013 Kevin Thornton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
